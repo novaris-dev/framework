0c05e605bbbfe5fad9f1e315a69adb54c8127c5d
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-10-16 16:32:20 -0700

    update Home Controller

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -17,105 +17,83 @@ use Novaris\Template\Tag\{DocumentTitle, Pagination};
 use Novaris\Tools\Str;
 use Symfony\Component\HttpFoundation\{Request, Response};
 
-class Home extends Controller
-{
+class Home extends Controller {
+
 	/**
 	 * Callback method when route matches request.
 	 *
 	 * @since 1.0.0
 	 */
-	public function __invoke( array $params, Request $request ): Response
-	{
+	public function __invoke( array $params, Request $request ): Response {
+
 		$types = App::resolve( 'content.types' );
 		$alias = Config::get( 'app.home_alias' );
-		$type  = false;
+		$collect = null;
 
-		// Checks if the homepage has an alias content type and if the
-		// type exists.
+		// Check if homepage alias exists and if type exists
 		if ( $alias && $types->has( $alias ) ) {
-			$type    = $types->get( $alias );
+
+			$type = $types->get( $alias );
 			$collect = $types->get( $type->collect() );
 		}
 
-		// If we have a content type and a collection type, run query.
-		if ( $type && $collect ) {
+		// Query if type and collection exist
+		if ( isset( $type, $collect ) ) {
 			$page = intval( $params['page'] ?? 1 );
 
-			// Query the content type.
+			// Query single content type
 			$single = Query::make( [
 				'path' => $type->path(),
 				'slug' => 'index'
 			] )->single();
 
-			// Merge the default collection query args for the type
-			// with user query args.
+			// Merge default collection query args with user-defined args
 			$query_args = array_merge(
 				$type->collectionArgs(),
 				$single ? $single->collectionArgs() : []
 			);
 
-			// Set required variables for the query.
 			$query_args['number'] = $query_args['number'] ?? 10;
-			$query_args['offset'] = $query_args['number'] * ( $page - 1 );
+			$query_args['offset'] = $query_args['number'] * ($page - 1);
 
-			// Query the content type collection.
-			$collection = Query::make( $query_args );
+			$collection = Query::make($query_args);
 
+			// Render if single and collection exist
 			if ( $single && $collection->all() ) {
-				$type_name = sanitize_slug( $type->type() );
-
-				$doctitle = new DocumentTitle( '', [
-					'page' => $page
-				] );
-
+				$doctitle = new DocumentTitle( '', ['page' => $page ] );
 				$pagination = new Pagination( [
 					'basepath' => '',
-					'current'  => $page,
-					'total'    => $collection->pages()
+					'current' => $page,
+					'total' => $collection->pages()
 				] );
 
 				return $this->response( $this->view(
 					Hierarchy::collectionHome( $single ),
-					[
-						'doctitle'   => $doctitle,
-						'pagination' => $pagination,
-						'single'     => $single,
-						'collection' => $collection
-					]
-				) );
+					compact( 'doctitle', 'pagination', 'single', 'collection' )
+				));
 			}
 		}
 
-		// Query the homepage `index.md` file.
-		$single = Query::make( [ 'slug' => 'index' ] )->single();
-
+		// Query homepage index file
+		$single = Query::make( ['slug' => 'index'] )->single();
 		if ( $single && $single->isPublic() ) {
-			$collection = false;
-
-			if ( $args = $single->collectionArgs() ) {
-				$collection = Query::make( $args );
-			}
+			$collection = $single->collectionArgs() ? Query::make( $single->collectionArgs() ) : null;
 
 			return $this->response( $this->view(
-				Hierarchy::singleHome( $single ),
-				[
-					'doctitle'   => new DocumentTitle(),
-					'pagination' => false,
-					'single'     => $single,
+				Hierarchy::singleHome($single), [
+					'doctitle' => new DocumentTitle(),
+					'pagination' => null,
+					'single' => $single,
 					'collection' => $collection
-				]
-			) );
+				] ) );
 		}
 
-		// If no index file is found, which is the minimum necessary for
-		// a site, we'll dump a notice and return an empty response.
-		// Note that this is not a 404. It is a user error.
+		// If no index file is found, display a notice and return an empty response
 		$notice = sprintf(
 			'No <code>%s</code> file found.',
-			Str::appendPath( App::get( 'path.content' ), 'index.md' )
+			Str::appendPath( App::get('path.content'), 'index.md' )
 		);
-
-		Message::make( $notice )->dump();
+		Message::make( $notice)->dump();
 
 		return new Response( '' );
 	}
