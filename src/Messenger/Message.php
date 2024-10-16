b9064dd1090b74ed3e56fd2935b497a31385e869
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-10-15 21:10:53 -0700

    initial commit

--- src/Messenger/Message.php
+++ src/Messenger/Message.php
@@ -8,16 +8,16 @@
  * In the long term, the plan is to scale this out for any type of system
  * messages, including custom ones.
  *
- * @package   Blush
- * @author    Justin Tadlock <justintadlock@gmail.com>
- * @copyright Copyright (c) 2018 - 2022, Justin Tadlock
- * @link      https://github.com/blush-dev/framework
- * @license   https://opensource.org/licenses/MIT
+ * @package   Novaris
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024. Benjamin Lu
+ * @link      https://github.com/novaris-dev/framework
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
  */
 
-namespace Blush\Messenger;
+namespace Novaris\Messenger;
 
-use Blush\Tools\Str;
+use Novaris\Tools\Str;
 
 class Message
 {
@@ -29,7 +29,7 @@ class Message
 	protected string $message = '';
 
 	/**
-	 * Type of message.  Appended as a class of `.blush-message--{$type}`.
+	 * Type of message.  Appended as a class of `.novaris-message--{$type}`.
 	 * The default supported types are `note`, `success`, `warning`, and
 	 * `error`.
 	 *
@@ -61,7 +61,7 @@ class Message
 	public function render(): string
 	{
 		$message = sprintf(
-			'<div class="blush-message blush-message--%s">%s</div>',
+			'<div class="novaris-message novaris-message--%s">%s</div>',
 			e( $this->type ?: 'note' ),
 			$this->message
 		);
@@ -110,13 +110,13 @@ class Message
 	protected function styles(): string
 	{
 		return '<style>
-		.blush-message {
-			--blush-message-spacing: 2rem;
-			--blush-message-color: #484a4c;
-			--blush-message-color-accent: #484a4c;
-			--blush-message-color-bg: #f4f9ff;
-			--blush-message-color-shadow: #e9f3f8;
-			--blush-message-color-shadow-text: #e9f3f8;
+		.novaris-message {
+			--novaris-message-spacing: 2rem;
+			--novaris-message-color: #484a4c;
+			--novaris-message-color-accent: #484a4c;
+			--novaris-message-color-bg: #f4f9ff;
+			--novaris-message-color-shadow: #e9f3f8;
+			--novaris-message-color-shadow-text: #e9f3f8;
 
 			clear:         both;
 			position:      relative;
@@ -126,47 +126,47 @@ class Message
 			width:         1024px;
 			max-width:     100%;
 			box-sizing:    border-box;
-			margin:        var( --blush-message-spacing ) auto;
-			padding:       var( --blush-message-spacing );
+			margin:        var( --novaris-message-spacing ) auto;
+			padding:       var( --novaris-message-spacing );
 
 			font-family:   \"Source Code Pro\", Monaco, Consolas, \"Andale Mono WT\", \"Andale Mono\", \"Lucida Console\", \"Lucida Sans Typewriter\", \"DejaVu Sans Mono\", \"Bitstream Vera Sans Mono\", \"Liberation Mono\", \"Nimbus Mono L\", \"Courier New\", Courier, monospace;
 			font-size:     18px;
 			line-height:   1.75;
-			color:         var( --blush-message-color );
-			text-shadow:   0 1px var( --blush-message-color-shadow-text );
-			background:    var( --blush-message-color-bg );
-			box-shadow:    inset 1px 1px 10px var( --blush-message-color-shadow );
+			color:         var( --novaris-message-color );
+			text-shadow:   0 1px var( --novaris-message-color-shadow-text );
+			background:    var( --novaris-message-color-bg );
+			box-shadow:    inset 1px 1px 10px var( --novaris-message-color-shadow );
 			border-radius: 6px;
-			border-left:   6px solid var( --blush-message-color-accent );
+			border-left:   6px solid var( --novaris-message-color-accent );
 		}
-		.blush-message--note {
-			--blush-message-color-accent: #2282bb;
-			--blush-message-color: #2282bb;
+		.novaris-message--note {
+			--novaris-message-color-accent: #2282bb;
+			--novaris-message-color: #2282bb;
 		}
-		.blush-message--success {
-			--blush-message-color-accent: #338d00;
+		.novaris-message--success {
+			--novaris-message-color-accent: #338d00;
 		}
-		.blush-message--error {
-			--blush-message-color-accent: #e23140;
+		.novaris-message--error {
+			--novaris-message-color-accent: #e23140;
 		}
-		.blush-message--warning {
-			--blush-message-color-accent: #d59401;
+		.novaris-message--warning {
+			--novaris-message-color-accent: #d59401;
 		}
-		.blush-message > * {
+		.novaris-message > * {
 			margin-top: 0;
 			margin-bottom: 0;
 		}
-		.blush-message > * + * {
-			margin-top: var( --blush-message-spacing );
+		.novaris-message > * + * {
+			margin-top: var( --novaris-message-spacing );
 			margin-bottom: 0;
 		}
-		.blush-message :first-child {
+		.novaris-message :first-child {
 			margin-top: 0;
 		}
-		.blush-message ul {
+		.novaris-message ul {
 			list-style-type: circle;
 		}
-		.blush-message code {
+		.novaris-message code {
 			font: inherit;
 			color: #484a4c;
 			padding: 0.125em 0.25em;
