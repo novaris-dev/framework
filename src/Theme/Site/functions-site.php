1e2d228a2c79d510216b42660e67ca448c21933f
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-10-30 07:52:50 -0700

    add tagline support

--- src/Theme/Site/functions-site.php
+++ src/Theme/Site/functions-site.php
@@ -56,6 +56,53 @@ function render_site_title( array $args = [] ): string {
 }
 
 /**
+ * Outputs the site description HTML for Novaris.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return void
+ */
+function display_site_description( array $args = [] ): void {
+
+    echo render_site_description( $args );
+}
+
+/**
+ * Returns the site description HTML for Novaris.
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array  $args
+ * @return string
+ */
+function render_site_description( array $args = [] ): string {
+
+    // Set default arguments
+    $args = array_merge( [
+        'tag'    => 'span',
+        'class'  => 'site-description',
+    ], $args );
+
+    // Retrieve the site description
+    $description = config( 'app.tagline' );
+
+    if ( !$description ) {
+        return ''; // Return empty if there is no description
+    }
+
+    // Construct the HTML
+    $html = sprintf(
+        '<%1$s class="%2$s">%3$s</%1$s>',
+        htmlspecialchars( $args['tag'], ENT_QUOTES, 'UTF-8' ),
+        htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
+        htmlspecialchars( $description, ENT_QUOTES, 'UTF-8' )
+    );
+
+    return $html;
+}
+
+/**
  * Outputs the site link HTML for Novaris.
  *
  * @since  1.0.0
