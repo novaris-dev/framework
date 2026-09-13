23396b479eaceb5b3751f7c8d55efddd118d0d18
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-13 13:06:54 -0700

    fix(application): prevent constant redefinition

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -74,12 +74,29 @@ class Application extends Container implements ApplicationContract, Bootable
 	 */
 	protected function registerDefaultConstants(): void
 	{
-		define( 'MINUTE_IN_SECONDS',  60                     );
-		define( 'HOUR_IN_SECONDS',    60 * MINUTE_IN_SECONDS );
-		define( 'DAY_IN_SECONDS',     24 * HOUR_IN_SECONDS   );
-		define( 'WEEK_IN_SECONDS',     7 * DAY_IN_SECONDS    );
-		define( 'MONTH_IN_SECONDS',   30 * DAY_IN_SECONDS    );
-		define( 'YEAR_IN_SECONDS',   365 * DAY_IN_SECONDS    );
+		if ( ! defined( 'MINUTE_IN_SECONDS' ) ) {
+			define( 'MINUTE_IN_SECONDS', 60 );
+		}
+
+		if ( ! defined( 'HOUR_IN_SECONDS' ) ) {
+			define( 'HOUR_IN_SECONDS', 60 * MINUTE_IN_SECONDS );
+		}
+
+		if ( ! defined( 'DAY_IN_SECONDS' ) ) {
+			define( 'DAY_IN_SECONDS', 24 * HOUR_IN_SECONDS );
+		}
+
+		if ( ! defined( 'WEEK_IN_SECONDS' ) ) {
+			define( 'WEEK_IN_SECONDS', 7 * DAY_IN_SECONDS );
+		}
+
+		if ( ! defined( 'MONTH_IN_SECONDS' ) ) {
+			define( 'MONTH_IN_SECONDS', 30 * DAY_IN_SECONDS );
+		}
+
+		if ( ! defined( 'YEAR_IN_SECONDS' ) ) {
+			define( 'YEAR_IN_SECONDS', 365 * DAY_IN_SECONDS );
+		}
 	}
 
 	/**
