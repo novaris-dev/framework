5ce86fe1efcf1691b0cc2bb041810d20c7f942a8
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-14 16:12:24 -0700

    feat(theme): add theme service provider

---
+++ src/Theme/ThemeServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Theme service provider.
+ *
+ * Registers theme-related services with the application container.
+ *
+ * @package   Novaris
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2026 Benjamin Lu
+ * @link      https://github.com/novaris-dev/framework
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Novaris\Theme;
+
+use Novaris\Core\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register theme services.
+	 *
+	 * @since 1.0.0
+	 */
+	public function register(): void
+	{
+		$this->app->singleton(
+			'theme.installer',
+			fn () => new Installer()
+		);
+
+		$this->app->singleton(
+			'theme.metadata',
+			fn () => new Metadata()
+		);
+	}
+}
