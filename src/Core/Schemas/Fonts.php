9318f93e806385678c06f30e69e3d3ed8cdf9edf
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-17 13:21:03 -0700

    feat(font): add fonts configuration schema

---
+++ src/Core/Schemas/Fonts.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Fonts configuration schema.
+ *
+ * @package   Novaris
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024. Benjamin Lu
+ * @link      https://github.com/novaris-dev/framework
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace Novaris\Core\Schemas;
+
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
+
+class Fonts
+{
+	/**
+	 * Returns the schema structure.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function schema(): Schema
+	{
+		return Expect::array( [] );
+	}
+}
