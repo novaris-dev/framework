6896b1f825907bd90fb23c73fe9aab1654e13c04
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-17 13:15:14 -0700

    feat(font): add font component

---
+++ src/Font/Component.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Font component.
+ *
+ * Manages the font component.
+ *
+ * @package   Novaris
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/novaris-dev/framework
+ */
+
+namespace Novaris\Font;
+
+use Novaris\Contracts\Bootable;
+
+/**
+ * Font component class.
+ *
+ * @since 1.0.0
+ */
+class Component implements Bootable
+{
+	/**
+	 * Stores the fonts collection.
+	 *
+	 * @since 1.0.0
+	 */
+	protected Fonts $fonts;
+
+	/**
+	 * Stores the font configuration.
+	 *
+	 * @since 1.0.0
+	 */
+	protected array $config;
+
+	/**
+	 * Creates the component object.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct( Fonts $fonts, array $config = [] )
+	{
+		$this->fonts  = $fonts;
+		$this->config = $config;
+	}
+
+	/**
+	 * Bootstraps the component.
+	 *
+	 * @since 1.0.0
+	 */
+	public function boot(): void
+	{
+		foreach ( $this->config as $id => $options ) {
+			$this->fonts->add( $id, $options );
+		}
+	}
+}
