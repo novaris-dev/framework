4d87f6f6752d12f2b7734648f0f8ef3c74abd9a5
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-18 10:31:56 -0700

    fix(view): correct view contract namespace

--- src/Contracts/View/View.php
+++ src/Contracts/View/View.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * View.
+ * View contract.
  *
- * Handles locating and rendering view templates.
+ * Defines the contract for view implementations.
  *
  * @package   Novaris
  * @author    Benjamin Lu <benlumia007@gmail.com>
@@ -11,197 +11,56 @@
  * @link      https://github.com/novaris-dev/framework
  */
 
-namespace Novaris\View;
+namespace Novaris\Contracts\View;
 
-use Novaris\Contracts\View\View as ViewContract;
-use Novaris\Tools\Collection;
-
-class View implements ViewContract
+interface View
 {
-	/**
-	 * View engine.
-	 *
-	 * @since 1.0.0
-	 */
-	protected Engine $engine;
-
-	/**
-	 * View name.
-	 *
-	 * @since 1.0.0
-	 */
-	protected string $name;
-
-	/**
-	 * View hierarchy.
-	 *
-	 * @since 1.0.0
-	 */
-	protected array $hierarchy = [];
-
-	/**
-	 * View data.
-	 *
-	 * @since 1.0.0
-	 */
-	protected Collection $data;
-
-	/**
-	 * Located template.
-	 *
-	 * @since 1.0.0
-	 */
-	protected ?string $template = null;
-
-	/**
-	 * Create a new view.
-	 *
-	 * @since 1.0.0
-	 */
-	public function __construct(
-		Engine $engine,
-		string $name,
-		array|string $hierarchy = [],
-		array|Collection $data = []
-	) {
-		$this->engine    = $engine;
-		$this->name      = $name;
-		$this->hierarchy = (array) $hierarchy;
-		$this->data      = $data instanceof Collection
-			? $data
-			: new Collection( $data );
-	}
-
 	/**
 	 * Get the view name.
 	 *
 	 * @since 1.0.0
 	 */
-	public function name(): string
-	{
-		return $this->name;
-	}
+	public function name(): string;
 
 	/**
 	 * Get the raw hierarchy slugs.
 	 *
 	 * @since 1.0.0
 	 */
-	public function slugs(): array
-	{
-		return $this->hierarchy;
-	}
+	public function slugs(): array;
 
 	/**
 	 * Get the view hierarchy.
 	 *
 	 * @since 1.0.0
 	 */
-	public function hierarchy(): array
-	{
-		$templates = [];
-
-		foreach ( $this->hierarchy as $template ) {
-			$templates[] = "{$this->name}/{$template}.php";
-		}
-
-		$default = "{$this->name}/default.php";
-
-		if ( ! in_array( $default, $templates, true ) ) {
-			$templates[] = $default;
-		}
-
-		return $templates;
-	}
+	public function hierarchy(): array;
 
 	/**
 	 * Locate the first available view template.
 	 *
-	 * Theme views take precedence over framework views.
-	 *
 	 * @since 1.0.0
 	 */
-	public function locate(): ?string
-	{
-		foreach ( $this->hierarchy() as $template ) {
-			$themeTemplate = theme_path(
-				"public/views/{$template}"
-			);
-
-			if ( is_file( $themeTemplate ) ) {
-				return $themeTemplate;
-			}
-
-			$frameworkTemplate = view_path( $template );
-
-			if ( is_file( $frameworkTemplate ) ) {
-				return $frameworkTemplate;
-			}
-		}
-
-		return null;
-	}
+	public function locate(): ?string;
 
 	/**
 	 * Get the located template.
 	 *
 	 * @since 1.0.0
 	 */
-	public function template(): ?string
-	{
-		if ( null === $this->template ) {
-			$this->template = $this->locate();
-		}
-
-		return $this->template;
-	}
+	public function template(): ?string;
 
 	/**
 	 * Display the view.
 	 *
 	 * @since 1.0.0
 	 */
-	public function display(): void
-	{
-		$template = $this->template();
-
-		if ( ! $template ) {
-			return;
-		}
-
-		extract(
-			$this->data->all(),
-			EXTR_SKIP
-		);
-
-		$data   = $this->data;
-		$view   = $this;
-		$engine = $this->engine;
-
-		include $template;
-	}
+	public function display(): void;
 
 	/**
 	 * Render the view.
 	 *
 	 * @since 1.0.0
 	 */
-	public function render(): string
-	{
-		ob_start();
-
-		$this->display();
-
-		return ob_get_clean();
-	}
-
-	/**
-	 * Render the view when converted to a string.
-	 *
-	 * @since 1.0.0
-	 */
-	public function __toString(): string
-	{
-		return $this->render();
-	}
+	public function render(): string;
 }
