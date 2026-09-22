1c88bfd4d262a40d2395ca37921b57c342792c3f
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-22 13:26:54 -0700

    converting to BEM

--- src/Theme/Site/functions-site.php
+++ src/Theme/Site/functions-site.php
@@ -1,151 +1,143 @@
 <?php
+/**
+ * Site template functions.
+ *
+ * @package   Novaris
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2024 Benjamin Lu
+ * @link      https://github.com/novaris-dev/framework
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
 namespace Novaris\Theme\Site;
 
 /**
  * Outputs the site title HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
- * @return void
+ * @since 1.0.0
+ *
+ * @param array $args Site title arguments.
  */
-function display_site_title( array $args = [] ): void {
-
-    echo render_site_title( $args );
+function display_site_title( array $args = [] ): void
+{
+	echo render_site_title( $args );
 }
 
 /**
  * Returns the site title HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
+ * @since 1.0.0
+ *
+ * @param  array $args Site title arguments.
  * @return string
  */
-function render_site_title( array $args = [] ): string {
+function render_site_title( array $args = [] ): string
+{
+	$args = array_merge( [
+		'tag'        => 'h1',
+		'class'      => 'branding__title',
+		'link_class' => 'branding__link'
+	], $args );
 
-    // Default arguments
-    $args = array_merge( [
-        'tag'        => 'h1',
-        'class'      => 'site-title',
-        'link_class' => 'site-title-link'
-    ], $args );
+	$title = config( 'app.title' );
 
-    // Placeholder for site title; replace with Novaris equivalent.
-    $title = config( 'app.title' );
+	if ( ! $title ) {
+		return '';
+	}
 
-    if ( !$title ) {
-        return ''; // Return empty if there's no title set.
-    }
+	$link = render_home_link( [
+		'text'  => $title,
+		'class' => $args['link_class']
+	] );
 
-    // Build the link for the site title.
-    $link = render_home_link( [
-        'text'  => $title,
-        'class' => $args['link_class']
-    ] );
-
-    // Construct the HTML.
-    $html = sprintf(
-        '<%1$s class="%2$s">%3$s</%1$s>',
-        htmlspecialchars( $args['tag'], ENT_QUOTES, 'UTF-8' ),
-        htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
-        $link
-    );
-
-    return $html;
+	return sprintf(
+		'<%1$s class="%2$s">%3$s</%1$s>',
+		htmlspecialchars( $args['tag'], ENT_QUOTES, 'UTF-8' ),
+		htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
+		$link
+	);
 }
 
 /**
  * Outputs the site description HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
- * @return void
+ * @since 1.0.0
+ *
+ * @param array $args Site description arguments.
  */
-function display_site_description( array $args = [] ): void {
-
-    echo render_site_description( $args );
+function display_site_description( array $args = [] ): void
+{
+	echo render_site_description( $args );
 }
 
 /**
  * Returns the site description HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
+ * @since 1.0.0
+ *
+ * @param  array $args Site description arguments.
  * @return string
  */
-function render_site_description( array $args = [] ): string {
+function render_site_description( array $args = [] ): string
+{
+	$args = array_merge( [
+		'tag'   => 'span',
+		'class' => 'branding__description'
+	], $args );
 
-    // Set default arguments
-    $args = array_merge( [
-        'tag'   => 'span',
-        'class' => 'site-description',
-    ], $args );
+	$description = config( 'app.tagline' );
 
-    // Retrieve the site description
-    $description = config( 'app.tagline' );
+	if ( ! $description ) {
+		return '';
+	}
 
-    if ( !$description ) {
-        return ''; // Return empty if there is no description
-    }
-
-    // Construct the HTML
-    $html = sprintf(
-        '<%1$s class="%2$s">%3$s</%1$s>',
-        htmlspecialchars( $args['tag'], ENT_QUOTES, 'UTF-8' ),
-        htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
-        htmlspecialchars( $description, ENT_QUOTES, 'UTF-8' )
-    );
-
-    return $html;
+	return sprintf(
+		'<%1$s class="%2$s">%3$s</%1$s>',
+		htmlspecialchars( $args['tag'], ENT_QUOTES, 'UTF-8' ),
+		htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
+		htmlspecialchars( $description, ENT_QUOTES, 'UTF-8' )
+	);
 }
 
 /**
- * Outputs the site link HTML for Novaris.
+ * Outputs the home link HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
- * @return void
+ * @since 1.0.0
+ *
+ * @param array $args Home link arguments.
  */
-function display_home_link( array $args = [] ): void {
-
-    echo render_home_link( $args );
+function display_home_link( array $args = [] ): void
+{
+	echo render_home_link( $args );
 }
 
 /**
- * Returns the site link HTML for Novaris.
+ * Returns the home link HTML for Novaris.
  *
- * @since  1.0.0
- * @access public
- * @param  array  $args
+ * @since 1.0.0
+ *
+ * @param  array $args Home link arguments.
  * @return string
  */
-function render_home_link( array $args = [] ): string {
+function render_home_link( array $args = [] ): string
+{
+	$args = array_merge( [
+		'text'   => '%s',
+		'class'  => 'branding__link',
+		'before' => '',
+		'after'  => ''
+	], $args );
 
-    // Set default arguments
-    $args = array_merge( [
-        'text'   => '%s',
-        'class'  => 'home-link',
-        'before' => '',
-        'after'  => ''
-    ], $args );
+	$home_url = e( uri() );
 
-    // Get the site URL
-    $home_url = e( uri() );
+	$html = sprintf(
+		'<a class="%s" href="%s" rel="home">%s</a>',
+		htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
+		$home_url,
+		htmlspecialchars( $args['text'], ENT_QUOTES, 'UTF-8' )
+	);
 
-    // Assume home URL and site name are handled in display_site_title
-    $html = sprintf(
-        '<a class="%s" href="%s" rel="home">%s</a>',
-        htmlspecialchars( $args['class'], ENT_QUOTES, 'UTF-8' ),
-        $home_url,
-        sprintf( $args['text'], htmlspecialchars( $args['text'], ENT_QUOTES, 'UTF-8' ) )
-    );
-
-    // Return the final HTML, including 'before' and 'after' elements
-    return $args['before'] . $html . $args['after'];
+	return $args['before'] . $html . $args['after'];
 }
 
 /**
