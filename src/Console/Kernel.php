5a8e7867029d8c7f9980e51c31487961af4fcc84
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-22 21:12:16 -0700

    update

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -87,8 +87,7 @@ class Kernel
 			$this->app->make( RoutingRouter::class ),
 			$this->app->make( ContentTypes::class ),
 			$this->app->make( ContentQuery::class ),
-			$this->app['path'] . '/dist',
-			$this->app['path'] . '/public'
+			$this->app['path'] . '/dist'
 		);
 
 		$exported = $exporter->export();
