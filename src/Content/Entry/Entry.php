bb49713b76d5a728f32ff067c9360087a89bf2a0
techkodeninja <164923145+techkodeninja@users.noreply.github.com>
2026-09-25 20:22:07 +0000

    updated

--- src/Content/Entry/Entry.php
+++ src/Content/Entry/Entry.php
@@ -4,7 +4,7 @@
  *
  * @package   Novaris
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2024. Benjamin Lu
+ * @copyright 2024 Benjamin Lu
  * @link      https://github.com/novaris-dev/framework
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  */
